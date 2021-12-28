62c36cbcf340b0c6783faa846891dda83f7f4baf
keshav-cruz <69050936+keshav-cruz@users.noreply.github.com>
2021-12-28 22:47:44 +0530

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,31 +1,40 @@
 
-function reloadon(){
+    // var xx="";
+    // var myInt = setInterval(reloadon,3000);
+    // document.addEventListener('DOMContentLoaded',function reloadon(){
+
     var x = document.getElementsByClassName("_6q-tv");
          x[0].innerHTML = "Hello World!";
          y=x[0].src
         y=y+"&dl=1";
     
-         z= document.getElementsByClassName("QGPIr");
+        //  z= document.getElementsByClassName("QGPIr");
 //     z[0].innerHTML="<a href="+y+" >click me</a>";
 // let li = document.createElement('span');
 // li.setAttribute('class','keshav');
 // li.textContent="<a href="+y+">Download DP</a>"
 // z.insertBefore(li, z.firstElementChild.nextSibling);
-
 // const pa = document.createElement("button");
 // const no = document.createTextNode("Download DP");
 // pa.appendChild(no)
 
-const para = document.createElement("a");
+// if(m!==xx){
+    // clearInterval(myInt);
+
+var para = document.createElement("a");
 para.setAttribute('href',y);
-para.setAttribute('class',"flatu");
-para.setAttribute('id',"faltu")
-const node = document.createTextNode("Download now");
+
+var node = document.createTextNode("Download now");
 para.appendChild(node);
-const element = document.getElementsByClassName("QGPIr")[0];
+var element = document.getElementsByClassName("QGPIr")[0];
 element.appendChild(para);
-}
-setInterval(reloadon,3000);
+// var xx=y; 
+// }
+  
+// }, false);
+
+
+
 
 
    
